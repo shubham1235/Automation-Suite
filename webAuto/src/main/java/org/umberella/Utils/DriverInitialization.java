98d2d04d873c035dd5c74f74ed6a862398462010
shubham verma <shubhamverma815@gmail.com>
2019-06-19 19:54:14 +0530

    Create DriverInitialization File

---
+++ webAuto/src/main/java/org/umberella/Utils/DriverInitialization.java
@@ -0,0 +1,42 @@
+package org.umberella.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+
+public class DriverInitialization {
+
+		
+	public static ChromeDriver setDriverPath() {
+
+		if (OSValidation.isMac()) {
+			System.out.println("Operation System :- Mac OS");
+			System.setProperty("webdriver.chrome.driver", "browserdriver/chromedriver");
+			
+		}
+		if (OSValidation.isWindows()) {
+			System.out.println("Operation System :- Windows OS");
+			System.setProperty("webdriver.chrome.driver", "browserdriver/chromedriver.exe");
+		}
+		if (OSValidation.isUnix()) {
+			System.out.println("Operation System :- Unix OS");
+			System.setProperty("webdriver.chrome.driver", "browserdriver/chromedriver_linux");
+		}
+	
+		Map<String, Object> prefs = new HashMap<String, Object>();
+
+		prefs.put("profile.default_content_setting_values.notifications", 1);
+
+		ChromeOptions options = new ChromeOptions();
+
+		options.setExperimentalOption("prefs", prefs);
+
+		ChromeDriver driver = new ChromeDriver(options);
+
+		return driver;
+	}
+}
